7cff2306f80675a1a33a0d52620325f4a585ab1e
Gideon Ekeke <71523437+gideonekekeke@users.noreply.github.com>
2020-10-19 16:20:00 +0100

    Add files via upload

---
+++ number.js
@@ -0,0 +1,59 @@
+// console.log(number.MAX_SAFE_INTEGER)
+
+
+
+// let num = 2**53 -1
+
+// console.log(num)
+
+
+
+// var a = 59
+// var b = 46
+// var c = 15
+// b++;
+
+
+// var result = (a === b)? "true":"false"
+// console.log(result)
+
+
+// let d = ((b%5 > 0))? "odd": "even"
+
+// console.log((d == "odd" && b> 10 && b>=(a*2)))
+
+
+
+
+let x = 67
+console.log(x.toString())
+console.log(typeof x.toString() )
+
+
+
+
+
+
+var a = 365e5
+console.log(a)
+
+
+
+var b = 36500000
+console.log(b.toExponential())
+
+
+var c = 6.785
+console.log(c.toFixed())
+console.log( (6 + 4 ) .valueOf())
+
+console.log(typeof parseInt(c))
+
+console.log(Math.random().toFixed(1) *10)
+//give the highest number
+console.log(Math.ceil(4.1))
+//floor gives the lowest number no rounding off
+console.log(Math.floor(4.5))
+// raise to the power of a number
+console.log(Math.pow(8 , 2))
+console.log(Math.sqrt(4));
